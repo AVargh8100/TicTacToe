08125dce335dbf56747eda1dc87d99195614715a
AVargh8100 <106758059+AVargh8100@users.noreply.github.com>
2024-07-02 20:09:00 -0400

    Add files via upload

---
+++ TicTacToe/TicTacToe/TTTImageView.swift
@@ -0,0 +1,26 @@
+//
+//  TTTImageView.swift
+//  TicTacToe
+//
+//  Created by Alex Varghese on 25/11/2023.
+//
+
+import UIKit
+
+class TTTImageView: UIImageView {
+    
+    var player: String?
+    var activated: Bool = false
+    
+    func setPlayer(_player: String) {
+        self.player = _player
+        DispatchQueue.main.async {
+            if self.activated == false, _player == "x" {
+                self.image = UIImage(named: "x")
+            } else {
+                self.image = UIImage(named: "o")
+            }
+            self.activated = true
+        }
+    }
+}
